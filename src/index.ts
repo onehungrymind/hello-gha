5b34fe8ec13d602494005564b37183e15a698f71
Lukas Ruebbelke <lukas@presolved.io>
2024-09-30 12:17:33 -0700

    update: incremental

--- src/index.ts
+++ src/index.ts
@@ -2,26 +2,14 @@ import { getInput } from '@actions/core';
 
 const name = getInput('name');
 
-(async () => {
-  console.log('HELLO FROM THE ASYNC BLOCK!')
-  console.log(await getPerson());
-})();
+async function exampleFetch() {
+  console.log('Fetching data...');
+  
+  const response = await fetch('https://swapi.dev/api/people/1/');
+  const json = await response.json();
+  console.log(json);
+}
+
+exampleFetch();
 
 console.log(`Hello, ${name}!`);
-
-const url = 'https://swapi.dev/api/';
-
-async function getPerson() {
-  const _url = `${url}people/1/`;
-
-  try {
-    const response = await fetch(_url);
-    if (!response.ok) {
-      throw new Error(`Response status: ${response.status}`);
-    }
-    const json = await response.json();
-    return json;
-  } catch (error: any) {
-    console.error(error.message);
-  }
-}

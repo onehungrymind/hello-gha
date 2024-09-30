4b8bfa2310fa358663bf41fd29aa7c7319f16a1f
Lukas Ruebbelke <lukas@presolved.io>
2024-09-30 12:10:14 -0700

    update: incremental

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,20 @@ import { getInput } from '@actions/core';
 const name = getInput('name');
 
 console.log(`Hello, ${name}!`);
+
+const url = 'https://swapi.dev/api/';
+
+async function getPerson() {
+  const _url = `${url}people/1/`;
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    const json = await response.json();
+    console.log(json);
+  } catch (error: any) {
+    console.error(error.message);
+  }
+}

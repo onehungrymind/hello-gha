4f9b06e2132174ed979f44bd71da6aa08673b6e7
Lukas Ruebbelke <lukas@presolved.io>
2024-09-30 12:19:14 -0700

    update: incremental

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,14 @@ const name = getInput('name');
 
 async function exampleFetch() {
   console.log('Fetching data...');
-  
+
   const response = await fetch('https://swapi.dev/api/people/1/');
   const json = await response.json();
   console.log(json);
 }
 
 exampleFetch();
+
+console.log('Hello Async!', exampleFetch);
 
 console.log(`Hello, ${name}!`);
